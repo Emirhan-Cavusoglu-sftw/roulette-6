214cfd65afd2f36e6663a1403a23503594b99f9c
Feyyaz Numan Cavlak <feyyazcavlak@outlook.com>
2024-03-23 01:07:26 +0300

    Add IBO

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,140 @@
+import {
+  FrameButton,
+  FrameContainer,
+  FrameImage,
+  FrameInput,
+  FrameReducer,
+  NextServerPageProps,
+  getFrameMessage,
+  getPreviousFrame,
+  useFramesReducer,
+} from "frames.js/next/server";
+import Link from "next/link";
+import { DEFAULT_DEBUGGER_HUB_URL, createDebugUrl } from "./debug";
+import { currentURL } from "./utils";
 
-export default function Home() {
+type State = {
+  active: string;
+  total_button_presses: number;
+};
+
+const initialState = { active: "1", total_button_presses: 0 };
+
+const reducer: FrameReducer<State> = (state, action) => {
+  return {
+    total_button_presses: state.total_button_presses + 1,
+    active: action.postBody?.untrustedData.buttonIndex
+      ? String(action.postBody?.untrustedData.buttonIndex)
+      : "1",
+  };
+};
+
+function getFrameImage(activePage: string, inputText?: string) {
+  switch (activePage) {
+    case "1":
+      return <FrameImage src="https://feyyum-web.vercel.app/ibrahim_tatlisesi_tehdit_etti_yemin_olsun_ben_bu_kani_dokecegim_1674967086_421.jpeg" />
+    case "2":
+      return <FrameImage aspectRatio="1.91:1"><div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>İbrahim Tatlısese mesajın: {inputText}</div></FrameImage>
+    case "3":
+      return <FrameImage src="https://feyyum-web.vercel.app/ibrahim_tatlisesi_tehdit_etti_yemin_olsun_ben_bu_kani_dokecegim_1674967086_421.jpeg" />
+    default:
+      return null
+  }
+}
+
+// This is a react server component only
+export default async function Home({ searchParams }: NextServerPageProps) {
+  const url = currentURL("/");
+  const previousFrame = getPreviousFrame<State>(searchParams);
+
+  const frameMessage = await getFrameMessage(previousFrame.postBody, {
+    hubHttpUrl: DEFAULT_DEBUGGER_HUB_URL,
+  });
+
+  // const frameMessage = await getFrameMessage(previousFrame.postBody);
+
+  if (frameMessage && !frameMessage?.isValid) {
+    throw new Error("Invalid frame payload");
+  }
+
+  const [state, dispatch] = useFramesReducer<State>(
+    reducer,
+    initialState,
+    previousFrame
+  );
+
+  // Here: do a server side side effect either sync or async (using await), such as minting an NFT if you want.
+  // example: load the users credentials & check they have an NFT
+
+  console.log("info: state is:", state);
+
+  // then, when done, return next frame
   return (
-   <>
-    <div>
-      Basic Frame Example
+    <div className="p-4">
+      frames.js starter kit. The Template Frame is on this page, it&apos;s in
+      the html meta tags (inspect source).{" "}
+      <Link href={createDebugUrl(url)} className="underline">
+        Debug
+      </Link>{" "}
+      or see{" "}
+      <Link href="/examples" className="underline">
+        other examples
+      </Link>
+      <FrameContainer
+        postUrl="/frames"
+        pathname="/"
+        state={state}
+        previousFrame={previousFrame}
+      >
+        {getFrameImage(state?.active, frameMessage?.inputText)}
+        {/* <FrameImage aspectRatio="1.91:1">
+          <div tw="w-full h-full bg-slate-700 text-white justify-center items-center flex flex-col">
+            <div tw="flex flex-row">
+              {frameMessage?.inputText ? frameMessage.inputText : "Hello world"}
+            </div>
+            {frameMessage && (
+              <div tw="flex flex-col">
+                <div tw="flex">
+                  Requester is @{frameMessage.requesterUserData?.username}{" "}
+                </div>
+                <div tw="flex">
+                  Requester follows caster:{" "}
+                  {frameMessage.requesterFollowsCaster ? "true" : "false"}
+                </div>
+                <div tw="flex">
+                  Caster follows requester:{" "}
+                  {frameMessage.casterFollowsRequester ? "true" : "false"}
+                </div>
+                <div tw="flex">
+                  Requester liked cast:{" "}
+                  {frameMessage.likedCast ? "true" : "false"}
+                </div>
+                <div tw="flex">
+                  Requester recasted cast:{" "}
+                  {frameMessage.recastedCast ? "true" : "false"}
+                </div>
+              </div>
+            )}
+          </div>
+        </FrameImage> */}
+        <FrameInput text="put some text here" />
+        <FrameButton>
+          {/* {state?.active === "1" ? "Active" : "Inactive"} */}
+          Vay vay
+        </FrameButton>
+        <FrameButton>
+          Vış Vış
+        </FrameButton>
+        <FrameButton>
+          Le le le
+        </FrameButton>
+        {/* <FrameButton action="link" target={`https://www.google.com`}>
+          External
+        </FrameButton> */}
+        <FrameButton action="link" target={`https://www.youtube.com/watch?v=v5eBn099zmw`}>
+          BAGLAMA!
+        </FrameButton>
+      </FrameContainer>
     </div>
-   </>
   );
 }
